ad0134efcdf96beea22ca9814826ed5f3c22ec84
Eli MacColl <elimaccoll@gmail.com>
2022-04-04 14:36:31 -0400

    removed debug print statements

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // const TUITS_API = "https://web-dev-server-sp22.herokuapp.com/api/tuits";
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
-console.log(TUITS_API);
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit);
